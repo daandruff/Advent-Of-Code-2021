2910ecbc6598831f72effa7f959194132d44cdca
Markus Kothe <markus.kothe@gmail.com>
2021-12-14 10:05:33 +0100

    Solve for part 2 day 14

--- day14.js
+++ day14.js
@@ -8,21 +8,78 @@ function step(data) {
     return transformedData;
 }
 
-function findCommon(data) {
-    let vals = []
+function findCommon(data, recievedVals = false) {
+    let vals = [];
     let lc = null;
     let mc = null;
 
-    [...data].forEach(v => { vals[v] = vals[v] ? vals[v]+1 : 1 });
+    if (recievedVals) {
+        vals = recievedVals;
+    } else {
+        [...data].forEach(v => { vals[v] = vals[v] ? vals[v]+1 : 1 });
+    }
+
     for (const [k, v] of Object.entries(vals)) { (lc === null) ? lc = v : (v < lc ? lc = v : lc) }
     for (const [k, v] of Object.entries(vals)) { (mc === null) ? mc = v : (v > mc ? mc = v : mc) }
 
     return { leastCommon: lc, mostCommon: mc };
 }
 
+function createList(data) {
+    let list = []
+    values = [...data];
+    values.forEach((v, i) => {
+        if (i !== 0) {
+            list[values[i-1] + v] = list[values[i-1] + v] ? list[values[i-1] + v] + 1 : 1;
+        }
+    });
+    return list;
+}
+
+function mutateList(data) {
+    let newList = [];
+
+    for (const [k, v] of Object.entries(data)) {
+        let a = k[0] + MODEL[k];
+        let b = MODEL[k] + k[1];
+
+        newList[a] = newList[a] ? newList[a] + v : v;
+        newList[b] = newList[b] ? newList[b] + v : v;
+    }
+
+    return newList;
+}
+
+function countChars(list) {
+    let charList = [];
+
+    for (const [k, v] of Object.entries(list)) {
+        [...k].forEach(c => { charList[c] = charList[c] ? charList[c] + v : v });
+    }
+
+    for (const [k, v] of Object.entries(charList)) {
+        charList[k] = Math.floor(v / 2);
+    }
+
+    // First and last character in initial data needs a bit of help... I can't bother to fix it the right way
+    charList['O']++;
+    charList['C']++;
+
+    return charList;
+}
+
+// Step one
 let workdata = DATA;
 for (let i = 0; i < 10; i++) {
     workdata = step(workdata)
 }
 let workCommon = findCommon(workdata);
 console.log("The quantity of the most common subtracted with the least common is: " + (workCommon.mostCommon - workCommon.leastCommon));
+
+// Step two, uses a totaly different approach
+let workList = createList(DATA);
+for (let i = 0; i < 40; i++) {
+    workList = mutateList(workList)
+}
+let listCommon = findCommon(workList, countChars(workList))
+console.log("The quantity of the most common subtracted with the least common is (40 Passes): " + (listCommon.mostCommon - listCommon.leastCommon));
